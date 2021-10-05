dd783b01ca6693b83a5f2244592611e194edb2f5
isakgranqvist2021 <isakwebdev@gmail.com>
2021-10-05 13:18:59 +0200

    board implementation start

--- client/src/Components/Game/Game.tsx
+++ client/src/Components/Game/Game.tsx
@@ -3,15 +3,49 @@
 import { useRef, useEffect, MutableRefObject } from 'react';
 import classes from 'Styles/game.module.css';
 
+const settings = {
+	width: 600,
+	height: 600,
+};
+
+const square = () => {};
+
 export default function Game(): JSX.Element {
 	const canvasRef: any = useRef<MutableRefObject<HTMLCanvasElement | null>>();
 
 	const init = (ctx: CanvasRenderingContext2D): void => {
-		console.log(ctx);
+		for (let i = 0; i < settings.width / 64; i++) {
+			// every 8th iteration increment by 1
+			let y = 0;
+
+			console.log(y);
+
+			if (i % 2 === 1) {
+				ctx.fillStyle = 'red';
+			} else {
+				ctx.fillStyle = 'yellow';
+			}
+
+			ctx.fillRect(
+				(settings.width / 8) * i,
+				y,
+				settings.width / 8,
+				settings.height / 8
+			);
+
+			console.log(i);
+		}
 	};
 
 	useEffect(() => {
-		return () => init(canvasRef.current.getContext('2d'));
+		let canvas = canvasRef.current;
+		canvas.width = settings.width;
+		canvas.height = settings.height;
+
+		let ctx = canvas.getContext('2d');
+		init(ctx);
+
+		return () => init(ctx);
 	}, [canvasRef]);
 
 	return <canvas ref={canvasRef} className={classes.canvas}></canvas>;
