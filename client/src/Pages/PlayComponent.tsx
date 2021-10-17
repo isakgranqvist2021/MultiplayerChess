cf663bf5ce5efd4d88f87c2328411c0056a5dddb
isakgranqvist2021 <isakwebdev@gmail.com>
2021-10-17 21:03:49 +0200

    server architecture implementation

--- client/src/Pages/PlayComponent.tsx
+++ client/src/Pages/PlayComponent.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import GameComponent from 'Components/GameComponent';
 import SidebarComponent from 'Components/SidebarComponent';
@@ -11,16 +11,32 @@ const Main = styled.div`
 `;
 
 export default function PlayComponent(): JSX.Element {
+	const socket: WebSocket = new WebSocket('ws://localhost:8080');
+
 	const [activeGame, setActiveGame] = useState<boolean>(false);
 	const { user, isLoading } = useAuth0();
-
-	if (isLoading) {
-		return <div>Loading ...</div>;
-	}
 
 	const startGame = () => {
 		setActiveGame(true);
 	};
+
+	useEffect(() => {
+		socket.onopen = () => {
+			console.log('ws connection open');
+		};
+
+		socket.onclose = () => {
+			console.log('ws connection closed');
+		};
+
+		socket.onmessage = () => {
+			console.log('ws connection message');
+		};
+	}, []);
+
+	if (isLoading) {
+		return <div>Loading ...</div>;
+	}
 
 	return (
 		<Main>
--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,10 @@ dotenv.config({
 	debug: true, // remove this line in production
 });
 
-import { server, app, wss, HOST, PORT } from './config';
+import { server, wss, HOST, PORT } from './config';
+import { connection } from './controllers/io';
+
+wss.on('connection', connection);
 
 server.listen(PORT, () => {
 	console.log(`Server listening on http://${HOST}:${PORT}`);
