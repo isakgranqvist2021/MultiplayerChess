ff86e12851b0b9ab23d03dfa26932374c9300a7a
isakgranqvist2021 <isakwebdev@gmail.com>
2021-10-19 18:57:23 +0200

    qwwqtqwt

--- client/src/Game/init.tsx
+++ client/src/Game/init.tsx
@@ -4,13 +4,30 @@ import { images, active } from './settings';
 import { Game } from 'js-chess-engine';
 import { createBoard } from 'Game/board';
 
+let file_names: any = {
+	// why is heroku not case sensetive ? '_'
+	b: 'bishop_w.svg',
+	k: 'king_w.svg',
+	n: 'knight_w.svg',
+	p: 'pawn_w.svg',
+	q: 'queen_w.svg',
+	r: 'rook_w.svg',
+	B: 'bishop_b.svg',
+	K: 'king_b.svg',
+	N: 'knight_b.svg',
+	P: 'pawn_b.svg',
+	Q: 'queen_b.svg',
+	R: 'rook_b.svg',
+};
+
 export const init = () => {
 	let game = new Game();
 	let pieces = createBoard();
 
 	for (let k in images) {
 		images[k].src =
-			process.env.REACT_APP_SERVER_ADDR + `/public/pieces/${k}.svg`;
+			process.env.REACT_APP_SERVER_ADDR +
+			`/public/pieces/${file_names[k]}`;
 	}
 
 	for (let k in active) {
