06fdf348124831b2309c97f2a9edb9e25d803a17
Isak Granqvist <isakwebdev@gmail.com>
2022-08-15 18:18:48 +0200

    Delete production directory

--- production/controllers/shared.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/** @format */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sockets = exports.rooms = void 0;
-exports.rooms = [];
-exports.sockets = [];
--- production/utils/sockets.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/** @format */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.emit = void 0;
-function emit() {
-    console.log('Emit');
-}
-exports.emit = emit;
